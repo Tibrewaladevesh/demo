46faae73bdf97a018cbcf35f5307570dc501ebee
Tibrewaladevesh <devesh.tibrewala@invansys.com>
2023-02-23 15:45:17 +0530

    Key value pair added in melon function

---
+++ dope/melon.js
@@ -0,0 +1,14 @@
+const melon = () => {
+    let obj = {
+        Firstname: "Devesh",
+        Lastname: "Tibrewala",
+        Mobile: 9899452111,
+        Gender: "Male",
+    }
+    for (let i in obj){
+        console.log(i)
+    }
+}
+
+melon();
+
